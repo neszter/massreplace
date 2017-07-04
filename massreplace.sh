38f1e171606f27303da4b67bf23c7f870744ce0e
mm-github <githubmm@gmail.com>
2017-07-04 16:25:03 +0100

    massreplace version by MM

--- massreplace.sh
+++ massreplace.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 #https://www.ibm.com/support/knowledgecenter/en/linuxonibm/liabo/liaboconcept.htm
-#in current directory to replace all foo with bar
-#sed -i -- 's/foo/bar/g' *
-#recursive incl hidden files
-#find . -type f -exec sed -i 's/foo/bar/g' {} +
+#
+#Author: Michele Modolo
+#Date: 04/07/2017
+#GIT-Project: massreplace (owned by my friend Eszter N.)
+#
+#This scripts lists all the files in a given directory and replace some strings with other strings in each file 
+#
+#In this example the script replaces "ICMNLSDB" with "ITALY" and "ICMADMIN" with "HUNGARY" within each file
+#The logic of this script is very clear (and can be reused for different purposes) and so is its syntax 
+
+for file in $(find . -type f)
+    do
+           sed 's/ICMNLSDB/ITALY/g; s/ICMADMIN/HUNGARY/g' $file > $file.tmp
+           mv $file.tmp $file
+done
